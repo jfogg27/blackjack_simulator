e32b61e04b274a83a51f64a6a4839ee569a2be78
jfogg27 <103511116+jfogg27@users.noreply.github.com>
2026-04-15 18:11:44 -0400

    Organized main_game and looped it with win tracking

--- main_game.py
+++ main_game.py
@@ -2,18 +2,20 @@ from random import shuffle
 
 CARDS = {"Ace": 11, "Two": 2, "Three": 3, "Four": 4,
 "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10}
+suit=['Ace',"Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
+win_loss=''
+win_loss_tracker=[]
+deck=[]
 dealer_cards=[]
 player_cards=[]
-suit=['Ace',"Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
-deck=[]
-deck.extend(suit*4)
+deck.extend(suit*1)
 shuffle(deck)
+loops=0
 
 def deal_with_aces(cards):
     temp=0
     for i in range(len(cards)):
                         temp+=CARDS[cards[i]]
-
     if temp>21:
         return 1
     else:
@@ -22,19 +24,15 @@ def deal_with_aces(cards):
 def calculate_score(cards):
     value=0
     for i in range(len(cards)):
+        
         if cards[i]!="Ace":
                         value+=CARDS[cards[i]]
+                        
         else:
             value+=deal_with_aces(cards)
-
-
-
-    print (cards)
-    print(value)
     return value
 
-
-def deal():
+def deal(deck):
     values=[]
     dealer_cards.append(deck[0])
     deck.pop(0)
@@ -47,8 +45,9 @@ def deal():
     deck.pop(0)
     values.append(calculate_score(player_cards))
     values.append(calculate_score(dealer_cards))
-    print(values)
+    
     return values
+    
 def hit(deck):
     values=[]
     player_cards.append(deck[0])
@@ -57,39 +56,49 @@ def hit(deck):
 
     values.append(calculate_score(player_cards))
     values.append(calculate_score(dealer_cards))
-
-    print(player_cards)
-    print(dealer_cards)
-    print(values)
-    check_game_state(values,deck)
+    return values
     
 def stand():
     print("standing")
-def hit_stand(player_cards,dealer_cards,deck):
-    i=''
-    i=input("h or s")
+    return 'l'
     
-    if i.lower()=="h":
-        hit(deck)
-    elif i.lower()=="s":
-        stand()
-        
-        
-values=deal()
-print(values[0])
-
-
-
-def check_game_state(values,deck):
-    length_deck=len(deck)
-    if length_deck<5:
+def hit_stand_func():
+    return input("h or s: ")
+def game_loop(deck):
+    if len(deck)<10:
         deck=[]
         deck.extend(suit*4)
         shuffle(deck)
+    hitting=True
+    values=deal(deck)
+    print("Dealer Cards: "+ dealer_cards[0]+', ? Value:'+str(CARDS[dealer_cards[0]]))
+    print("Your Cards: "+player_cards[0]+","+player_cards[1]+" Value: "+ str(values[0]))
+    print(len(deck))
+    
+    
+    while hitting:
+        hit_or_stand=hit_stand_func()
         
-    if values[0]<21:
-        hit_stand(player_cards,dealer_cards,deck)
-    elif values[0]>21:
-        stand()
-        
-check_game_state(values,deck)
+        if hit_or_stand=='h':
+            values=hit(deck)
+            print("Dealer Cards: "+ dealer_cards[0]+', ? Value:'+str(CARDS[dealer_cards[0]]))
+            print("Your Cards:")
+            
+            for i in range(len(player_cards)):
+              print(player_cards[i-1])
+            print('Value: '+str(values[0]))
+            
+        elif hit_or_stand=='s':
+            hitting=False
+            
+    return stand()
+    
+while loops<5:
+    dealer_cards=[]
+    player_cards=[]
+    win_loss=game_loop(deck)
+    win_loss_tracker.extend(win_loss)
+    print(win_loss_tracker)   
+    loops+=1
+
+
